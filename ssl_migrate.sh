8f45863797a060dcb67745b27361baa5b6120d46
K <x00.x7f@gmail.com>
2016-12-20 20:25:57 +0900

    Modified ssl deadline 10 years to 100 years

--- ssl_migrate.sh
+++ ssl_migrate.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+
+mkdir -p ./ssl
+cd ./ssl
 openssl genrsa -out laputa.key 2048
 openssl req -new -key laputa.key -sha256 -out laputa.csr
-openssl x509 -in laputa.csr -days 3650 -req -signkey laputa.key -sha256 -out laputa.crt
+openssl x509 -in laputa.csr -days 36500 -req -signkey laputa.key -sha256 -out laputa.crt
